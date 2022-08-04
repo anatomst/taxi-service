2628bd0128a7caf5f82adffc4b7b7dd728ea96de
Anton Shpachuk <www.shpachyk98@gmail.com>
2022-08-04 19:56:36 +0300

    s

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-8ovil3xu6=eaoqd#-#&ricv159p0pypoh5_lgm*)-dfcjqe=yc")
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+
+# django-insecure-8ovil3xu6=eaoqd#-#&ricv159p0pypoh5_lgm*)-dfcjqe=yc
 
 # SECURITY WARNING: don"t run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
@@ -28,7 +30,7 @@ DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 ALLOWED_HOSTS = ["127.0.0.1", "taxi-app-service.herokuapp.com"]
 
 INTERNAL_IPS = [
-    "127.0.0.1",
+    "127.0.0.1", "taxi-app-service.herokuapp.com"
 ]
 
 # Application definition
